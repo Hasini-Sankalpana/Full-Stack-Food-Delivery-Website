258841d98fca17e7e3c3923ba5cb9c959ba03fec
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-06-23 16:03:34 +0530

    Complete Sign in / Sign up Component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("home");
   return (
@@ -29,7 +29,7 @@ const Navbar = () => {
 
                    </div>
                 </div>
-                <button>Sign In</button>
+                <button onClick={() => setShowLogin(true)}>Sign In</button>
             </div>
     </div>
   )
