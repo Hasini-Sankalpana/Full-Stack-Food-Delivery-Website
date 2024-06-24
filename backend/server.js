4a23c605f50a14ee603f9478dd961df2b4b2d50e
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-06-24 17:22:30 +0530

    Complete user authentication (login & registration) feature

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodrouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config';
 
 
 //app config
@@ -21,7 +23,9 @@ connectDB();
 //api endpoints
 
 app.use("/api/food",foodrouter);
-app.use("/images",express.static('uploads'))
+app.use("/images",express.static('uploads'));
+app.use("/api/user",userRouter);
+
 
 app.get("/", (req,res) => {
     res.send("API Working");
