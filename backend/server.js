1a0a63b57309f631ee5ece6378e4f7103541ca2e
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-06-25 21:50:59 +0530

    Complete place order feature and stripe payment integration

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import foodrouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import 'dotenv/config';
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 
 //app config
@@ -27,6 +28,7 @@ app.use("/api/food",foodrouter);
 app.use("/images",express.static('uploads'));
 app.use("/api/user",userRouter);
 app.use("/api/cart",cartRouter);
+app.use("/api/order",orderRouter);
 
 
 app.get("/", (req,res) => {
